c95e74ddeb6363363f77be4bcfc75edc7562b0b9
JadeLuke <jadeluke.booysen@gmail.com>
2024-07-09 09:38:31 +0200

    added min date to calendar

--- style.js
+++ style.js
@@ -1,25 +1,29 @@
 let data = [];
+let today = new Date();
+let day = today.getDate();
+let month = today.getMonth()+1; 
+let year = today.getFullYear();
+ if(day<10){
+        day='0'+day
+    } 
+    if(month<10){
+        month='0'+month
+    } 
 
-// function displayFn(){
-// let userTask = document.getElementById('task').value;
-// let priority = document.getElementById('priority').value 
-// let date = document.getElementById('deadline').value
-   
-// let text = document.getElementById('display');
-// text.innerHTML = userTask + priority + date;
+today = year+'-'+month+'-'+day;
+document.getElementById("deadline").setAttribute("min", today);
 
-// }
+
 function readAll (){
 let taskList = document.getElementById('display')
 let elements = "";
 data.forEach((d, index) => {
     elements += `
-    <form id="form">
     <div class="card">
     <div class="heading">
     <h5> Task:
     </h5>
-    <p>${d.userTask}</p>
+    <p id="taskData">${d.userTask}</p>
     </div><br>
     <div class="Priorities">
     <h5>Priority:</h5>
@@ -30,10 +34,8 @@ data.forEach((d, index) => {
     ${d.date}
     </div>
     <button class="btnDel" onclick="removeTask(this)">Delete</button>
-    <button class="btnEdit" onclick="changeTask(this)" id="edt">Edit</button>
-
     </div>
-    </form>`
+    `
 })
 taskList.innerHTML = elements;
 document.getElementById('task').value = ""
@@ -42,39 +44,24 @@ document.getElementById('deadline').value = ""
 
 function add(){
     let userTask = document.getElementById('task').value;
-    let priority = document.getElementById('priority').value 
-    let date = document.getElementById('deadline').value
+    let priority = document.getElementById('priority').value; 
+    let date = document.getElementById('deadline').value;
+    
 
-    if (userTask !== " "){
-      let newObject = {userTask, priority, date};
-      data.push(newObject);
-      readAll();
-
-    } else {
-        alert('No')
+    if (userTask == "" || userTask == " " || date == ""){
+      alert('Please enter a task or date')
+    } 
+    else {
+       let newObject = {userTask, priority, date};
+       data.push(newObject);
+       localStorage.setItem('items', JSON.stringify(newObject));
+       readAll();
     }
 }
 
 function removeTask(delTask) {
  delTask.parentElement.remove();
  data.splice(delTask.parentElement.id, 1);
-}
-
-
-const element = document.getElementById('edt')
-element.addEventListener("click", changeTask)
-
-// function editTask (edit){
-// let selectedTask = edit.parentElement;
-// let userTask = document.getElementById('task').value;
-// userTask.value = selectedTask.children[0].innerHTML;
-
-
-function changeTask(edit){
-document.getElementById('form').preventDefault()
-userTask.value = edit.innerHTML;
-// edit.parentElement.parentElement.remove();
-// remove() 
-
+ localStorage.removeItem('items');
 
 }
